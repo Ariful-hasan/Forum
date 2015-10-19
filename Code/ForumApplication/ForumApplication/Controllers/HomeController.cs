eda8e94c692f53aad0db21b62d0a040ee0385a73
ashikur11186741 <ashik.rafi@hotmail.com>
2015-10-19 17:23:07 +0600

    initial commit

---
+++ Code/ForumApplication/ForumApplication/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using ForumApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForumApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ViewResult FAQ()
+        {
+            //ViewBag.Message = "Frequently ask question any time.";
+            ViewBag.Country = new List<string>()
+            {
+                "India",
+                "USA",
+                "UK",
+                "UAE"
+            };
+
+            return View();    
+        }
+
+        public ActionResult MyCustomPage()
+        {
+            var model = new UserModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult MyCustomPage(UserModel user)
+        {
+            user.StatusMessage = "Recieved and saved.";
+            return View(user);
+            //return Content("Received Username:" + user.username + " Age:" + user.age);
+        }
+    }
+}
---
+++ Code/ForumApplication/ForumApplication/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForumApplication.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string Age { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
